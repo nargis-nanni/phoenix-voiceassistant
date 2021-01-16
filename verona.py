eba595c0130e0beda7da09e85d58ac8312381b9a
kajal984 <kajalsexana9@gmail.com>
2021-01-16 15:40:09 +0530

    voice assistant rename

--- verona.py
+++ verona.py
@@ -39,15 +39,15 @@ def speak(audio):
 def wishMe():
     hour = int(datetime.datetime.now().hour)
     if hour>= 0 and hour<12:
-        speak("Good Morning Sir !")
+        speak("Good Morning nargis !")
   
     elif hour>= 12 and hour<18:
-        speak("Good Afternoon Sir !")   
+        speak("Good Afternoon nargis !")   
   
     else:
-        speak("Good Evening Sir !")  
+        speak("Good Evening nargis !")  
   
-    assname =("Jarvis 1 point o")
+    assname =("Verona 1 point o")
     speak("I am your Assistant")
     speak(assname)
 
